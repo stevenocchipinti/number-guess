5e6f8018d398485941a3406e31a412aadc7cb5a0
Steve Occhipinti <dev@stevenocchipinti.com>
2017-05-03 17:41:24 +1000

    Add comments for exercises

--- README.md
+++ README.md
@@ -3,10 +3,24 @@ Number Guess
 
 A simple number guessing game in Ruby
 
+This should be a starting point for beginners to practice their programming
+skills with some simple exercises.
+
+
+Exercises
+---------
+
+At the top of `app.rb` there are some comments with exercises to complete.
+
 
 How to run it
 -------------
 
+Clone this repo, open the directory in a terminal and type:
+
 ```
 ruby app.rb
 ```
+
+Alternatively, if you don't know how to use `git`, you could just download (or
+copy/paste) `app.rb` instead of cloning.
--- app.rb
+++ app.rb
@@ -1,7 +1,35 @@
-require 'io/console'
+################################################################################
+#
+#                        Number Guessing Game Exercises
+#                        ------------------------------
+#
+#
+# Exercises:
+#
+#   1. When the player guesses correctly, show a message saying:
+#        "Well done! You got it right!"
+#      and when the player guesses incorrectly, show a message saying:
+#        "Oops, nice try"
+#
+#   2. When the player guesses a smaller number, show a message saying:
+#        "Nope.... higher"
+#      and when the player guesses a larger number, show a message saying:
+#        "Nope.... lower"
+#
+#   3. Let the player have 5 guesses
+#
+#
+# Extension Exercises:
+#
+#   1. Let the player have unlimited number of guesses, make sure they have the
+#   "Guess: " prompt everytime.
+#
+#   2. Allow the user to type "exit" to quit the program
+#
+################################################################################
 
 print "Player 1, pick a number: "
-number = STDIN.noecho(&:gets)
+number = gets
 
 puts
 puts "Player 2, try to guess the number!"
