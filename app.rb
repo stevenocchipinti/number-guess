8309cd9f1599ef5f1a8604a238ab0a9c772dc939
Steve Occhipinti <steve.occhipinti@rea-group.com>
2017-05-11 00:08:53 +1000

    Add more exercises!
    Ditching the idea of extension exercises at this point because so far
    there is only one project here.
    
    If there is eventually separate exercises for different topics, I think
    having a set of basic exercises while providing the more advanced
    students a set of extension exercises will work well.

--- app.rb
+++ app.rb
@@ -18,13 +18,17 @@
 #
 #   3. Let the player have 5 guesses
 #
+#   4. Let the player have unlimited number of guesses, make sure they have the
+#   "Guess: " prompt everytime
 #
-# Extension Exercises:
+#   5. Allow the user to type "exit" to quit the program
 #
-#   1. Let the player have unlimited number of guesses, make sure they have the
-#   "Guess: " prompt everytime.
+#   6. When the game has ended, show the user how many guesses it took them
+#   (before they won or quit the game)
 #
-#   2. Allow the user to type "exit" to quit the program
+#   7. Make this a single player game by starting with a random number between
+#   0-20 that the player has to try to guess - don't forget to change the
+#   messages to the user to make that clear
 #
 ################################################################################
 
